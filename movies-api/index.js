2d5ac909471d4ce9c51d48b8d221b90d7f05f3e3
Hermes Aguilar <adanhermes23@gmail.com>
2021-07-30 10:52:23 -0500

    funciona puta madre git

---
+++ movies-api/index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+
+const app = express();
+
+const { config } = require('./config/index');
+const moviesApi = require('./routes/movies.js');
+const moviesAPI = require('./routes/movies.js')
+
+//middleware body parser
+app.use(express.json());
+
+moviesApi(app);
+
+
+app.listen(config.port, function(){
+    console.log(`Listening http://localhost:${config.port}`)
+})
